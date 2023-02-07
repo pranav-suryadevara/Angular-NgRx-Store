5c08fe6b9b460db166a32bf7e0698dd8654cf6c1
Pranav <pranav.suryadevara11@gmail.com>
2023-02-07 18:42:34 +0530

    NgRx Reducers

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -2,12 +2,27 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
   MetaReducer,
+  on,
 } from "@ngrx/store";
+import { AuthActions } from "../action-types";
+import { User } from "../model/user.model";
 
 export const authFeatureKey = "auth";
 
-export interface State {}
+export interface AuthState {
+  user: User;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const initialAuthState: AuthState = { user: undefined };
+
+// function authReducer(state, action): AuthState { } // crude way
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return { user: action.user };
+  })
+);

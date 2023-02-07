df6243d77ad2a1c2f064602f88f015e83ace1e73
Pranav <pranav.suryadevara11@gmail.com>
2023-02-07 17:17:30 +0530

    The Store Service API

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,19 +1,14 @@
-import { isDevMode } from '@angular/core';
+import { isDevMode } from "@angular/core";
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
-} from '@ngrx/store';
+  MetaReducer,
+} from "@ngrx/store";
 
-export interface State {
+export interface AppState {}
 
-}
+export const reducers: ActionReducerMap<AppState> = {};
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];

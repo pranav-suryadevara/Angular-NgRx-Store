7331995966fc99b792a195a2073e52cd98a8dcbc
Pranav <pranav.suryadevara11@gmail.com>
2023-02-10 12:37:54 +0530

    NgRx Meta Reducers

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -21,8 +21,6 @@ export const initialAuthState: AuthState = { user: undefined };
 export const authReducer = createReducer(
   initialAuthState,
   on(AuthActions.login, (state, action) => {
-    // state.user = action.user;
-    // return state; // this implementation breaks the time travelling debuggers. Breaks onPush ChangeDetection
     return { user: action.user };
   }),
   on(AuthActions.logout, (state, action) => {
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -12,4 +12,16 @@ export interface AppState {}
 
 export const reducers: ActionReducerMap<AppState> = { router: routerReducer };
 
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log("state before: ", state);
+    console.log("Action", action);
+
+    return reducer(state, action); // passing the action and state to the next reducers in the chain.
+  };
+}
+
+export const metaReducers: MetaReducer<AppState>[] = isDevMode()
+  ? [logger]
+  : []; // adding the reducers in the develop environment.
+// metaReducers are going to be executed in the order they are put in this array.

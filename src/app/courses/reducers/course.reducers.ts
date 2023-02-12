20ec468f9e70eee5acdcb0693872df2dca6752c9
Pranav <pranav.suryadevara11@gmail.com>
2023-02-12 06:11:50 +0530

    Let’s remove the loading from the home component and use the app component load

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -3,19 +3,22 @@ import { createReducer, on } from "@ngrx/store";
 import { CourseActions } from "../action-types";
 import { compareCourses, Course } from "../model/course";
 
-export interface CoursesState extends EntityState<Course> {}
+export interface CoursesState extends EntityState<Course> {
+  allCoursesLoaded: boolean;
+}
 
 export const adapter = createEntityAdapter<Course>({
   sortComparer: compareCourses,
-  // selectId: (course) => course.courseId, if the id property is defined by a custom identifier instead of id as the default case
 });
 
-export const initialCoursesState = adapter.getInitialState();
+export const initialCoursesState = adapter.getInitialState({
+  allCoursesLoaded: false,
+});
 
 export const coursesReducer = createReducer(
   initialCoursesState,
   on(CourseActions.allCoursesLoaded, (state, action) =>
-    adapter.setAll(action.courses, state)
+    adapter.setAll(action.courses, { ...state, allCoursesLoaded: true })
   )
 );
 
